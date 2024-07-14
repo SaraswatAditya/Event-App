bd8e11f2b634e0d03c636a0cf0555395e6ebb779
SaraswatAditya <adityasaraswat92@gmail.com>
2024-07-14 18:18:00 +0530

    Verecel backend

--- server/server.js
+++ server/server.js
@@ -13,33 +13,36 @@ const app = express();
 /* middleware */
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({
-  origin: ['*','https://event-app-sand-eta.vercel.app','https://event-app-aditya-saraswats-projects.vercel.app/', 'https://event-backend-one.vercel.app'], // Add your frontend URL here
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
 app.use(morgan("tiny"));
 app.disable("x-powered-by"); // less hackers know about our stack
 
-const port = process.env.PORT;
-// console.log("Post is :", port);
+const port = process.env.PORT || 8080;
 
 // HTTP GET Request
 app.get("/", (req, res) => {
   res.status(201).json("Home GET Request");
 });
 
-// api route
+// API routes
 app.use("/api", router);
 app.use("/api/events", eventRoutes);
 
-// start server when have valid connection
+// Start server when there is a valid connection
 connect()
   .then(() => {
     try {
       app.listen(port, () => {
-        console.log(`Server Connected to http://localhost:${port}`);
+        console.log(`Server connected to http://localhost:${port}`);
       });
     } catch (error) {
       console.log("Cannot connect to the server");
